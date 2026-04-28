0c80c00fa70111954a4e9740a0552288062ff3bd
Swidnixx <swidnixx@gmail.com>
2026-04-28 18:16:00 +0200

    World Scorller tiles prefabs

--- Assets/Scripts/WorldScroller.cs
+++ Assets/Scripts/WorldScroller.cs
@@ -2,20 +2,29 @@ using UnityEngine;
 
 public class WorldScroller : MonoBehaviour
 {
-    public Transform tileLeft, tileRight;
+    public SpriteRenderer tileLeft, tileRight;
+
+    public SpriteRenderer[] tilePrefabs;
 
     private void Update()
     {
-        tileLeft.position += new Vector3(GameManager.Instance.worldScrollingSpeed * -Time.deltaTime, 0, 0);
-        tileRight.Translate(new Vector3(GameManager.Instance.worldScrollingSpeed * -Time.deltaTime, 0, 0));
+        tileLeft.transform.position += new Vector3(GameManager.Instance.worldScrollingSpeed * -Time.deltaTime, 0, 0);
+        tileRight.transform.Translate(new Vector3(GameManager.Instance.worldScrollingSpeed * -Time.deltaTime, 0, 0));
 
-        if(tileRight.position.x <= 0)
+        if(tileRight.transform.position.x <= 0)
         {
-            tileLeft.position += new Vector3(24.05f * 2f, 0, 0);
+            SpriteRenderer newTile = Instantiate(
+                tilePrefabs[Random.Range(0, tilePrefabs.Length)], transform
+                );
 
-            Transform tmp = tileLeft;
+            float leftTileWidth = newTile.bounds.size.x;
+            float rightTileWidth = tileRight.bounds.size.x;
+            newTile.transform.position = tileRight.transform.position 
+                + new Vector3(rightTileWidth /2 + leftTileWidth /2, 0, 0);
+
+            Destroy(tileLeft.gameObject);
             tileLeft = tileRight;
-            tileRight = tmp;
+            tileRight = newTile;
         }
     }
 }
